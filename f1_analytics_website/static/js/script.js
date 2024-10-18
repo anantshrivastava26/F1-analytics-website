71f47e9b923503969cd1cd59fdfcfb80567a7b6b
Anant Shrivastava <143089789+anantshrivastava26@users.noreply.github.com>
2024-10-18 19:18:35 +0530

    Add files via upload
    UI update

--- f1_analytics_website/static/js/script.js
+++ f1_analytics_website/static/js/script.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("JavaScript is loaded and ready.");
 
-    // You can add any JavaScript logic here, for example, form validation.
-    
-    // Example: Simple form validation for analysis type selection
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        const select = document.getElementById('analysis_type');
-        if (select.value === '') {
-            event.preventDefault();
-            alert('Please select an analysis type before submitting.');
-        }
-    });
+    // Function to handle card clicks and submit form
+    window.submitForm = function(analysisType) {
+        const analysisInput = document.getElementById('analysis_type');
+        analysisInput.value = analysisType;
+        document.getElementById('analysisForm').submit();
+    };
 });
